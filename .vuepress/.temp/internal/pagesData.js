5963eacaa4522be982aa5199acf2f1344687ac35
13akstjq <13akstjq@gmail.com>
2022-11-25 22:19:31 +0900

    nextjs deploy

--- .vuepress/.temp/internal/pagesData.js
+++ .vuepress/.temp/internal/pagesData.js
@@ -5,34 +5,34 @@ export const pagesData = {
   "v-3e214e9b": () => import(/* webpackChunkName: "v-3e214e9b" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/About/index.html.js").then(({ data }) => data),
   // path: /docs/API/2022-11-24-unsplash-API-%EC%82%AC%EC%9A%A9%ED%95%B4%EC%84%9C-%EB%9E%9C%EB%8D%A4-%EC%82%AC%EC%A7%84-%EB%B6%88%EB%9F%AC%EC%98%A4%EA%B8%B0/
   "v-32d21ace": () => import(/* webpackChunkName: "v-32d21ace" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/API/2022-11-24-unsplash-API-사용해서-랜덤-사진-불러오기/index.html.js").then(({ data }) => data),
-  // path: /docs/Git/2022-11-25-github-repository%EC%97%90-remote-origin-%EC%97%B0%EA%B2%B0%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
-  "v-39f3c26a": () => import(/* webpackChunkName: "v-39f3c26a" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Git/2022-11-25-github-repository에-remote-origin-연결하는-방법/index.html.js").then(({ data }) => data),
+  // path: /docs/CSS/border-animation/
+  "v-0505b5b6": () => import(/* webpackChunkName: "v-0505b5b6" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/CSS/border-animation/index.html.js").then(({ data }) => data),
+  // path: /docs/CSS/imageset/
+  "v-475c08a1": () => import(/* webpackChunkName: "v-475c08a1" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/CSS/imageset/index.html.js").then(({ data }) => data),
   // path: /docs/Javascript/optional-chaining/
   "v-0fd7d848": () => import(/* webpackChunkName: "v-0fd7d848" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Javascript/optional-chaining/index.html.js").then(({ data }) => data),
   // path: /docs/Nextjs/2022-11-24-Nextjs%EC%97%90%EC%84%9C-svg-%EC%82%AC%EC%9A%A9%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
   "v-2e70fa45": () => import(/* webpackChunkName: "v-2e70fa45" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/2022-11-24-Nextjs에서-svg-사용하는-방법/index.html.js").then(({ data }) => data),
   // path: /docs/Nextjs/2022-11-25-Nextjs-Static-HTML-%EC%82%AC%EC%9D%B4%ED%8A%B8-%EB%B0%B0%ED%8F%AC%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
   "v-6b21e1c4": () => import(/* webpackChunkName: "v-6b21e1c4" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/2022-11-25-Nextjs-Static-HTML-사이트-배포하는-방법/index.html.js").then(({ data }) => data),
-  // path: /docs/Nextjs/getStaticProps-getServerSideProps/
-  "v-43071ee6": () => import(/* webpackChunkName: "v-43071ee6" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/getStaticProps-getServerSideProps/index.html.js").then(({ data }) => data),
-  // path: /docs/Nextjs/2022-11-25-React-Hook-useEffect-has-missing-dependencies-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95/
-  "v-591df59c": () => import(/* webpackChunkName: "v-591df59c" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/2022-11-25-React-Hook-useEffect-has-missing-dependencies-해결-방법/index.html.js").then(({ data }) => data),
   // path: /docs/Nextjs/2022-11-25-Nextjs-build-%EC%97%90%EB%9F%AC-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95/
   "v-0585c2bb": () => import(/* webpackChunkName: "v-0585c2bb" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/2022-11-25-Nextjs-build-에러-해결-방법/index.html.js").then(({ data }) => data),
+  // path: /docs/Nextjs/2022-11-25-React-Hook-useEffect-has-missing-dependencies-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95/
+  "v-591df59c": () => import(/* webpackChunkName: "v-591df59c" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/2022-11-25-React-Hook-useEffect-has-missing-dependencies-해결-방법/index.html.js").then(({ data }) => data),
+  // path: /docs/Git/2022-11-25-github-repository%EC%97%90-remote-origin-%EC%97%B0%EA%B2%B0%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95/
+  "v-39f3c26a": () => import(/* webpackChunkName: "v-39f3c26a" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Git/2022-11-25-github-repository에-remote-origin-연결하는-방법/index.html.js").then(({ data }) => data),
+  // path: /docs/Nextjs/getStaticProps-getServerSideProps/
+  "v-43071ee6": () => import(/* webpackChunkName: "v-43071ee6" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/getStaticProps-getServerSideProps/index.html.js").then(({ data }) => data),
   // path: /docs/Nextjs/zustand-react-hydration-error/
   "v-36b0ea94": () => import(/* webpackChunkName: "v-36b0ea94" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Nextjs/zustand-react-hydration-error/index.html.js").then(({ data }) => data),
   // path: /docs/Selenium/2022-11-24-Selenium-chrome-version-%EC%9D%B4%EC%8A%88/
   "v-e94584a2": () => import(/* webpackChunkName: "v-e94584a2" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Selenium/2022-11-24-Selenium-chrome-version-이슈/index.html.js").then(({ data }) => data),
-  // path: /docs/Vuepress/vuepress-hosting/
-  "v-d8df1a64": () => import(/* webpackChunkName: "v-d8df1a64" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Vuepress/vuepress-hosting/index.html.js").then(({ data }) => data),
   // path: /docs/Setting/m1-nvm/
   "v-19ae13b9": () => import(/* webpackChunkName: "v-19ae13b9" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Setting/m1-nvm/index.html.js").then(({ data }) => data),
   // path: /docs/Vuepress/hits/
   "v-64390c90": () => import(/* webpackChunkName: "v-64390c90" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Vuepress/hits/index.html.js").then(({ data }) => data),
-  // path: /docs/CSS/border-animation/
-  "v-0505b5b6": () => import(/* webpackChunkName: "v-0505b5b6" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/CSS/border-animation/index.html.js").then(({ data }) => data),
-  // path: /docs/CSS/imageset/
-  "v-475c08a1": () => import(/* webpackChunkName: "v-475c08a1" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/CSS/imageset/index.html.js").then(({ data }) => data),
+  // path: /docs/Vuepress/vuepress-hosting/
+  "v-d8df1a64": () => import(/* webpackChunkName: "v-d8df1a64" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/docs/Vuepress/vuepress-hosting/index.html.js").then(({ data }) => data),
   // path: /404.html
   "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"/Users/user/Desktop/Blog/ui-log.github.io/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
 }
--- docs/Nextjs/2022-11-25-Nextjs-Static-HTML-사이트-배포하는-방법/README.md
+++ docs/Nextjs/2022-11-25-Nextjs-Static-HTML-사이트-배포하는-방법/README.md
@@ -1,7 +1,7 @@
 ---
 title: Nextjs Static HTML 사이트 배포하는 방법 (feat. Netlify)
 description: Nextjs Static HTML site를 Netlify에 deploy하는 방법에 대해서 소개합니다.
-date: 2022-11-25 17:02
+date: 2022-11-25 22:02
 sidebarDepth: 2
 tag: Nextjs
 thumbnail:
@@ -9,7 +9,7 @@ thumbnail:
 
 # Nextjs Static HTML 사이트 배포하는 방법
 
-::: tip 목표
+::: tip 💡목표
 웹사이트를 만들다보면 DB를 사용하지 않는 정적 웹 사이트를 만드는 경우가 있는데요.
 Nextjs를 활용해 정적 웹사이트 배포하는 방법에 대해서 소개합니다.
 정적 웹 사이트 배포는 Netlify를 통해서 진행합니다.

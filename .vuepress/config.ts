b95a372a524a3827ea2f07fabfdc67226fcf4419
13akstjq <13akstjq@gmail.com>
2022-12-07 11:24:22 +0900

    ga 추가

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -87,6 +87,7 @@ export default defineUserConfig({
     ["meta", { name: "msapplication-config", content: "/browserconfig.xml" }],
     ["meta", { name: "theme-color", content: "#ffffff" }],
     ["script", { async: true, crossorigin: "anonymous", src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4877378276818686" }],
+    ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-VYKVD4LR7J" }],
   ],
   extendsMarkdown: (md) => {
     md.use(MarkdownItPlantuml);
